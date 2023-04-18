3673fd68b0b1fb36d17ceaf100f8a10d1fc1528e
MZadonski <maria.zadons@tktk.ee>
2023-04-18 20:56:27 +0300

    add files

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Thats my comment
+// rakendus kysib kasutajalt sisestada enda nime
+// rakendus tervitab kasutaja nimepidi
+
+Console.WriteLine("Enter yor name");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + "," + userName + "!");
+
+Console.WritenLine($"Hello, {userName} !");
+
